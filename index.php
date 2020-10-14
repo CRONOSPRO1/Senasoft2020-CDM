82c80755d944cf654e589b3c7b0accc112d3b311
CRONOSPRO1 <arevalojuan48@gmail.com>
2020-10-14 09:33:30 -0500

    Documentacion

--- index.php
+++ index.php
@@ -1,32 +1,3 @@
-<?php /*require_once'modelo/conexion.php'; 
-require_once'config/parameters.php';
-
-
-$controller='Usuarios';
-if(!isset($_REQUEST['c']))
-{
-    require_once "controller/$controller.Controller.php";
-    $controller = ucwords($controller) . 'Controller';
-    $controller = new $controller;
-    $controller->Index();    
-}
-else
-{
-    // Obtenemos el controlador que queremos cargar
-    $controller = strtolower($_REQUEST['controller']);
-    $accion = isset($_REQUEST['action']) ? $_REQUEST['action'] : 'Index';
-
-    // Instanciamos el controlador
-    require_once "controller/$controller.controller.php";
-    $controller = ucwords($controller) . 'Controller';
-    $controller = new $controller;
-
-    // Llama la accion
-    call_user_func( array( $controller, $accion ) );
-}
-*/
-
-?>
 <?php 
 require_once 'modelo/conexion.php';
 require_once 'autoload.php';
